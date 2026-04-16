c933a1e78e8e90cc21f614a0d8749b667c64a8b8
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-04-16 09:51:37 +0530

    ha test

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -4,6 +4,6 @@ module "linux_ha" {
   resource_group_name = "rg-prod"
   location            = "eastus"
   environment         = "prod"
-  vm_count            = 1
+  vm_count            = 2
   admin_password      = var.admin_password
 }
